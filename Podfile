aa726a7bb6a63d220c58dc50eb324595e4db657c
Maarten Zonneveld <maarten@safeguardapp.nl>
2018-02-07 18:49:17 +0100

    Set up pods

---
+++ Podfile
@@ -0,0 +1,8 @@
+
+platform :ios, '10.0'
+use_frameworks!
+
+target 'Disconnect' do
+    pod 'Fabric'
+	pod 'Crashlytics'
+end
---
+++ Pods/Target Support Files/Pods-Disconnect/Pods-Disconnect-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Disconnect : NSObject
+@end
+@implementation PodsDummy_Pods_Disconnect
+@end
